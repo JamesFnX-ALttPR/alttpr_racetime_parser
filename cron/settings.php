aec37a83c1cd24589295b7b236112a7fa1b24bbc
James <139070656+JamesFnX-ALttPR@users.noreply.github.com>
2023-07-09 13:34:44 -0700

    Add files via upload

---
+++ cron/settings.php
@@ -0,0 +1,9 @@
+<?php
+// Fill in your MySQL server, username, password, and database information. The ro (read-only) user should only have permissions to select on the database.
+$server = "";
+$userro = "";
+$passro = "";
+$user = "";
+$pass = "";
+$db = "";
+?>
