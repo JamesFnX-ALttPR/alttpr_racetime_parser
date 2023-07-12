230b3dd52ee54ec4e0d2cfe1eb7448dd9dd934ac
James <139070656+JamesFnX-ALttPR@users.noreply.github.com>
2023-07-12 15:57:40 -0700

    Update faq.php
    Changing static links to dynamic links

--- faq.php
+++ faq.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
+<?php
+function getRequestURL() {
+  if (isset($_SERVER['HTTPS']) &&
+  ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+  isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $protocol = 'https://';
+  } else {
+    $protocol = 'http://';
+  }
+  $url = $protocol . $_SERVER['HTTP_HOST'];
+  return $url;
+}
+?>
 <html>
   <head>
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="<?php echo getRequestURL(); ?>/styles.css">
     <title>RaceTime.GG Async Search/Submission Tool by JamesFnX</title>
   </head>
   <body>
-    <div style="width: 50%; margin-left: auto; margin-right: auto;"><span class="headerleft"><a href="https://projects.thecleanupstep.com/">Home</a></span><span class="headerright"><a href="https://projects.thecleanupstep.com/discord" target="_blank">Discord</a></span></div>
+    <div style="width: 50%; margin-left: auto; margin-right: auto;"><span class="headerleft"><a href="<?php echo getRequestURL(); ?>/">Home</a></span><span class="headerright"><a href="<?php echo getRequestURL(); ?>/discord" target="_blank">Discord</a></span></div>
     <br><br><hr>
     <h1>Frequently Asked Questions</h1>
     <div><input type="button" value="What does this site do?" id="faq1" onclick="toggleAnswer1(this)" class="faq_question"></div><br>
@@ -17,11 +31,11 @@
     <div><input type="button" value="How do I compare my time to other runners?" id="faq4" onclick="toggleAnswer4(this)" class="faq_question"></div><br>
     <div style="display:none;" id="answer4" class="faq_answer">Next to each race (or on the Permalink page) there's a link to a form to input your name, time, and comments. Comments are subject to admin review and may be deleted without warning. Don't be a jerk.</div><br>
     <div><input type="button" value="I don't see a race I did recently here - where is it?" id="faq5" onclick="toggleAnswer5(this)" class="faq_question"></div><br>
-    <div style="display:none;" id="answer5" class="faq_answer">One of three things happened: the race was not rolled by Sahasrahbot; the race <em>was</em> rolled by Sahasrahbot and our script didn't detect it; or our script hasn't gotten new race data since the race was completed. If the race was rolled by Sahasrahbot and the seed URL is public and we don't have the race, post a bug report in our <a href="https://projects.thecleanupstep.com/discord" target="_blank">Discord</a> and we'll take a look.</div><br>
+    <div style="display:none;" id="answer5" class="faq_answer">One of three things happened: the race was not rolled by Sahasrahbot; the race <em>was</em> rolled by Sahasrahbot and our script didn't detect it; or our script hasn't gotten new race data since the race was completed. If the race was rolled by Sahasrahbot and the seed URL is public and we don't have the race, post a bug report in our <a href="<?php echo getRequestURL(); ?>/discord" target="_blank">Discord</a> and we'll take a look.</div><br>
     <div><input type="button" value="How do I know people who've posted async times aren't cheating?" id="faq6" onclick="toggleAnswer6(this)" class="faq_question"></div><br>
     <div style="display:none;" id="answer6" class="faq_answer">You don't and we don't either. This site is not an arbiter of truth. We won't be posting leaderboards of race winners and any statistics we release will be solely on RaceTime data.</div><br>
     <div><input type="button" value="I have an idea for a new feature. How can I tell you?" id="faq7" onclick="toggleAnswer7(this)" class="faq_question"></div><br>
-    <div style="display:none;" id="answer7" class="faq_answer">We've got a channel for feature requests on the <a href="https://projects.thecleanupstep.com/discord" target="_blank">Discord</a>. This is a hobby and a learning project for me (I'm not a developer or coder by trade and I haven't written lots of HTML/PHP in probably 15 years, so new features may be slow to release at times because I'm trying to figure out how to do what you want.)</div><br>
+    <div style="display:none;" id="answer7" class="faq_answer">We've got a channel for feature requests on the <a href="<?php echo getRequestURL(); ?>/discord" target="_blank">Discord</a>. This is a hobby and a learning project for me (I'm not a developer or coder by trade and I haven't written lots of HTML/PHP in probably 15 years, so new features may be slow to release at times because I'm trying to figure out how to do what you want.)</div><br>
     <div><input type="button" value="I'd like to help out with development of the site. How can I get access?" id="faq8" onclick="toggleAnswer8(this)" class="faq_question"></div><br>
     <div style="display:none;" id="answer8" class="faq_answer">I'll have a GitHub of the site <em>Soon™</em> but until then, reach out to JamesFnX on the Discord.</div><br>
     <div><input type="button" value="How else can we help out?" id="faq9" onclick="toggleAnswer9(this)" class="faq_question"></div><br>
