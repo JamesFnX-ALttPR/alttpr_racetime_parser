8b24ee57b63c8152c351e1005e408198fbc01950
James <139070656+JamesFnX-ALttPR@users.noreply.github.com>
2023-08-03 10:07:59 -0700

    Update ca_display.php

--- ca_display.php
+++ ca_display.php
@@ -5,51 +5,55 @@ $domain = getRequestURL();
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Custom Asyncs</title>
-    <link rel="stylesheet" href=<?php echo '"' . $domain; ?>/styles.css">
-  </head>
-  <body>
+	<head>
+		<title>Custom Asyncs</title>
+		<link rel="stylesheet" href=<?php echo '"' . $domain; ?>/styles.css">
+	</head>
+	<body>
 <?php
 if(!isset($_GET['id'])) {
-        die('No Race ID provided!');
+	die('No Race ID provided!');
 } else {
-        $id = $_GET['id'];
+	$id = $_GET['id'];
 }
 $conn = new mysqli($server,$userro,$passro,$dbdev);
 if($conn->connect_error) {
-  die('Connection failed: ' . $conn->connect_error);
+	die('Connection failed: ' . $conn->connect_error);
 }
 $st1 = $conn->prepare("SELECT name, description FROM custom_async WHERE id = ?");
 $st1->bind_param("i", $id);
 $st1->execute();
 $rslt1 = $st1->get_result();
 while($rw1 = $rslt1->fetch_assoc()) {
-        $name = $rw1['name'];
-        $description = $rw1['description'];
+	$name = $rw1['name'];
+	$description = $rw1['description'];
 }
-echo '    <div style="width: 50%; margin-left: auto; margin-right: auto; text-align: center;"><span class="headerleft"><a href="' . $domain . '">Home</a></span><span class="headercenter"><a href="' . $domain . '/featured">Featured Modes</a></span><span class="headerright"><a href="' . $domain . '/faq">FAQ</a></span></div>' . PHP_EOL;
-echo '    <br><br><hr>' . PHP_EOL;
-echo '    <h1>Custom Asyncs for Tournaments and Communities</h1>' . PHP_EOL;
-echo '    <h2 style="text-align: center; font-family: \'Hylia Serif Beta\'; color: white">' . $name . '</h2>'. PHP_EOL;
-echo '    <p style="text-align: center;">' . $description . '</p><hr>' . PHP_EOL;
-echo '    <table style="width: 90%;">' . PHP_EOL;
-$st = $conn->prepare("SELECT id, seed, mode, hash FROM custom_seeds WHERE async_id = ?");
+echo '		<div style="width: 50%; margin-left: auto; margin-right: auto; text-align: center;"><span class="headerleft"><a href="' . $domain . '">Home</a></span><span class="headercenter"><a href="' . $domain . '/featured">Featured Modes</a></span><span class="headerright"><a href="' . $domain . '/faq">FAQ</a></span></div>' . PHP_EOL;
+echo '		<br><br><hr>' . PHP_EOL;
+echo '		<h1>Custom Asyncs for Tournaments and Communities</h1>' . PHP_EOL;
+echo '		<h2 style="text-align: center; font-family: \'Hylia Serif Beta\'; color: white">' . $name . '</h2>'. PHP_EOL;
+echo '		<p style="text-align: center;">' . $description . '</p><hr>' . PHP_EOL;
+echo '		<table style="width: 90%;">' . PHP_EOL;
+$st = $conn->prepare("SELECT id, seed, mode, hash, coop FROM custom_seeds WHERE async_id = ?");
 $st->bind_param("i", $id);
 $st->execute();
 $rslt = $st->get_result();
-echo '      <tr><th>Seed</th><th>Mode</th><th>Hash</th><th>Players</th><th colspan="2">View/Submit Times</th></tr>' . PHP_EOL;
+echo '			<tr><th>Seed</th><th>Mode</th><th>Hash</th><th>Players</th><th colspan="2">View/Submit Times</th></tr>' . PHP_EOL;
 while($rw = $rslt->fetch_assoc()) {
-        $st2 = $conn->prepare("SELECT id FROM custom_times WHERE seed_id = ?");
-        $st2->bind_param("i", $rw['id']);
-        $st2->execute();
-        $rslt2 = $st2->get_result();
-        $players = $rslt2->num_rows;
-        $st2->close();
-        echo '      <tr><td style="text-align: center;"><a target="_blank" href="' . htmlentities($rw['seed']) . '">Seed Link</a></td><td style="text-align: center;">' . $rw['mode'] . '</td><td style="text-align:center;">' . hashToImages($rw['hash']) . '</td><td style="text-align: center;">' . $players . '</td><td style="text-align: center;"><form action="' . $domain . '/customasync/submittime" method="post"><input type="hidden" id="race_id" name="race_id" value="' . $rw['id'] . '"><input type="submit" value="Submit Time"></form></td><td style="text-align: center;"><form action="' . $domain . '/ca_viewtimes.php" method="get"><input type="hidden" id="race_id" name="race_id" value="' . $rw['id'] . '"><input type="submit" value="View Times"></form></td></tr>' . PHP_EOL;
+	$st2 = $conn->prepare("SELECT id FROM custom_times WHERE seed_id = ?");
+	$st2->bind_param("i", $rw['id']);
+	$st2->execute();
+	$rslt2 = $st2->get_result();
+	$players = $rslt2->num_rows;
+	$st2->close();
+	echo '			<tr><td style="text-align: center;"><a target="_blank" href="' . htmlentities($rw['seed']) . '">Seed Link</a></td><td style="text-align: center;">';
+	if($rw['coop'] == 'y'){
+		echo 'Co-Op - ';
+	}
+	echo $rw['mode'] . '</td><td style="text-align:center;">' . hashToImages($rw['hash']) . '</td><td style="text-align: center;">' . $players . '</td><td style="text-align: center;"><form action="' . $domain . '/customasync/submittime" method="post"><input type="hidden" id="race_id" name="race_id" value="' . $rw['id'] . '"><input type="submit" value="Submit Time"></form></td><td style="text-align: center;"><form action="' . $domain . '/ca_viewtimes.php" method="get"><input type="hidden" id="race_id" name="race_id" value="' . $rw['id'] . '"><input type="submit" value="View Times"></form></td></tr>' . PHP_EOL;
 }
-echo "    </table>" . PHP_EOL;
+echo "		</table>" . PHP_EOL;
 $st->close();
 ?>
-  </body>
+	</body>
 </html>
